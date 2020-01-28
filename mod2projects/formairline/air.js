f93f19a3dec02e1b3bcd974282456832dc34809b
zavian greene <zgreenejr@icloud.com>
2020-01-28 12:37:03 -0700

    this push is to save my airline project and to keep my projects up to date

---
+++ mod2projects/formairline/air.js
@@ -0,0 +1,26 @@
+const form = document.flight
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+   
+    const firstN = form.first.value
+    const lastn = form.last.value
+    const agge = form.age.value
+    const Genn = form.gender.value
+    const city = form.cities.value
+    const foo = form.foods.value
+    form.first.value = ""
+    form.last.value = ""
+    form.age.value = ""
+    
+    
+    // const h1 = document.createElement('h1')
+    
+    // h1.textContent = firstN + " " + lastn + " " +agge
+    // h1.textContent = `${firstN} ${lastn} ${agge}`
+    window.alert(`First Name: ${firstN}\n Last Name: ${lastn}\n Age: ${agge}\n Gender: ${Genn}\n City: ${city} Meal: ${foo}`)
+
+    
+    document.getElementsByTagName("body")[0].append(h1)
+})
+
